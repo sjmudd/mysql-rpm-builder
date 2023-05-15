a9a6bf6d4aceb21e6beada4267b8913e84d16c53
Simon J Mudd <sjmudd@pobox.com>
2023-05-15 13:04:15 +0200

    Adjust to use common routines

--- build-environment.sh
+++ build-environment.sh
@@ -2,6 +2,38 @@
 #
 # build environment for building MySQL from src.rpms
 #
+
+# Install the appropriate src.rpm from upstream sources
+# - keep local copy to speed up process so if we do this frequently
+install_srpms () {
+    local SRPMS="$1" # space separated list of src.rpms to install from urls
+
+	for url in $SRPMS; do
+		rpm=$(basename $url)
+		if [ ! -e /data/SRPMS/$rpm ]; then
+			echo "Downloading $url to /data/SRPMS"
+			( cd /data/SRPMS && wget $url )
+		fi
+		location=/data/SRPMS/$rpm
+		echo "Installing $url from $location"
+	done
+}
+
+# build package
+# - FIXME (add signing)
+# - FIXME fix config to work with OS other than rhel8
+# - commercial 0 should not be needed as should be default (?)
+rpmbuild_rpms () {
+    local timestamp=$(date +%Y%m%d.%M%H%S.log)
+    cd ~/rpmbuild/SPECS
+    rpmbuild --define 'commercial 0' --define 'el8 1' --define 'rhel 8' -ba mysql.spec 2>&1 | tee -a mysql-build-$build_environment.$timestamp.log
+    rc=$?
+    # if build is successful record the installed package list
+    if [ $rc = 0 ]; then
+        rpm -qa > rpm-qa.$timestamp
+    fi
+}
+
 BUILD_USER=rpmbuild
 
 set -e
--- config/8.0.32/build.sh
+++ config/8.0.32/build.sh
@@ -6,21 +6,6 @@
 build () {
 	SRPMS="https://yum.oracle.com/repo/OracleLinux/OL8/MySQL80/community/x86_64/getPackageSource/mysql-community-8.0.32-1.el8.src.rpm"
 
-	# Install the appropriate src.rpm from upstream sources
-	# - keep local copy to speed up process is we do this frequently
-	for url in $SRPMS; do
-		rpm=$(basename $url)
-		if [ ! -e /data/SRPMS/$rpm ]; then
-			echo "Downloading $url to /data/SRPMS"
-			( cd /data/SRPMS && wget $url )
-		fi
-		location=/data/SRPMS/$rpm
-		echo "Installing $url from $location"
-	done
-
-	# build package
-	# - FIXME (add signing)
-	# - commercial 0 should not be needed as should be default (?)
-	cd ~/rpmbuild/SPECS
-	rpmbuild --define 'commercial 0' --define 'el8 1' --define 'rhel 8' -ba mysql.spec
+    install_srpms "$SRPMS"
+    rpmbuild_rpms
 }
