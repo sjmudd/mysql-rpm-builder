b33f8534e4ae6181ff2498bbde88f6b4b7118a7f
Simon J Mudd <sjmudd@pobox.com>
2023-05-15 18:15:26 +0200

    Add information on how to run with other compatible docker images

--- start-docker-container.sh
+++ start-docker-container.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-docker run --rm -it --network=host --hostname=mysql-builder -v $PWD:/data quay.io/centos/centos:stream8
+# other images:
+# - quay.io/centos/centos:stream8 (DEFAULT)
+# - oraclelinux:8.7
+image=${1:-quay.io/centos/centos:stream8}
+
+echo "Starting mysql-rpm-builder using image: $image"
+docker run --rm -it --network=host --hostname=mysql-builder -v $PWD:/data $image
